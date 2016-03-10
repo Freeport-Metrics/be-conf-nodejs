5f19bd43805cc4d2ebd08a6e28d4f8db8622179e
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2016-03-10 14:04:30 +0100

    Env configuration for openshift - fix

--- config/env_config.js
+++ config/env_config.js
@@ -1,10 +1,9 @@
 /**
  * Created by Matuszewski on 10/03/16.
  */
-var env = process.env
 
 module.exports = function(server){
-  if(env == 'staging'){
+  if(process.env.NODE_ENV == 'staging'){
     server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
       console.log(`Application worker ${process.pid} started... port ${env.NODE_PORT}`);
     });;
