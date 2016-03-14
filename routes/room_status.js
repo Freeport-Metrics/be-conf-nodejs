ac3be610cfc88452dd85206ada2d0d5fed68e176
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2016-03-14 09:59:38 +0100

    Add slack integration to node.js server

--- routes/room_status.js
+++ routes/room_status.js
@@ -1,8 +1,10 @@
 /**
  * Created by Matuszewski on 10/03/16.
  */
+var room_status = require('../services/room_service')
+
 function room_status(req, res) {
-  res.send('');
+  res.send(JSON.stringify(room_status,null,2));
 }
 
 module.exports = function (app) {
