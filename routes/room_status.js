57cbf4e22fe50b093a002b3470ec6c5af03e6c56
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2016-03-14 11:19:43 +0100

    Added room status formatting for command

--- routes/room_status.js
+++ routes/room_status.js
@@ -4,7 +4,28 @@
 var rooms = require('../storage/room_status')();
 
 function room_status(req, res) {
-  res.send(JSON.stringify(rooms,null,2));
+  res.send(formatResponse());
+}
+
+function formatResponse(){
+  var response = "";
+  rooms.rooms.forEach(function(room,index){
+    response += "========" + "</br>"
+    response +=  "<strong>" + room.label + "</strong>" + "</br>"
+    response +=  "Users inside: <strong>" + room.users.length + "</strong>" + "</br>"
+    if (room.users.length > 0){
+      response +=  "Users : "
+      room.users.forEach(function(user, index){
+        response += " " + "<strong>" + user.name + "</strong>"
+      });
+      response += "</br>"
+      response += "Status: <strong>OCCUPIED</strong> </br>"
+    }else{
+      response += "Status: <strong>FREE</strong> </br>"
+    }
+    response += "========" + "</br>"
+  })
+  return response
 }
 
 module.exports = function (app) {
