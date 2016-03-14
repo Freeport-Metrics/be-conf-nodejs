7408996533de27b8e74d2ff31df20ef30265899d
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2016-03-14 12:23:40 +0100

    update formatting for returning message

--- routes/room_status.js
+++ routes/room_status.js
@@ -16,15 +16,15 @@ function formatResponseForSlack(){
   rooms.rooms.forEach(function(room,index){
     var occupied = room.users.length > 0;
     var occupied_icon = occupied ? ":red_circle:" :":white_circle:";
-    var occupied_status = occupied ? "OCCUPIED" : "FREE";
-    slack_response += occupied_icon + " " +  room.label + " is currently " + occupied_status
+    var occupied_status = occupied ? "*OCCUPIED*" : "*FREE*";
+    slack_response += occupied_icon + " " +  "*"+room.label+"*" + " is currently " + occupied_status
     if(occupied){
       slack_response += " by "
       room.users.forEach(function(user, index){
-        slack_response += user.name
+        slack_response += " " + "*"+user.name+"*"
       });
     }
-    slack_response += "/n"
+    slack_response += " \n"
   })
   return slack_response;
 }
