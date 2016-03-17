19291d41439bada52ecd2def4b0d92b962d247e6
Tomasz Matuszewski <matuszewski@freeportmetrics.com>
2016-03-17 16:07:24 +0100

    Testing cron job setting

--- jobs/room_cleanup_job.js
+++ jobs/room_cleanup_job.js
@@ -5,5 +5,5 @@
 var CronJob = require('cron').CronJob;
 
 module.exports = function(task){
-  new CronJob('00 00 15 * * 1-5', task, true,  'Europe/Warsaw');
+  new CronJob('00 09 11 * * 1-5', task, true,  'America/New_York');
 }
